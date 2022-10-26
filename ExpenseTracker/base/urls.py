9d71ef875e4788e9a24b574fb5ae164645172a5e
Kevin Yuan <kyuan23@wooster.edu>
2022-10-26 15:28:19 -0400

    Pre Authentication

--- ExpenseTracker/base/urls.py
+++ ExpenseTracker/base/urls.py
@@ -1,6 +1,13 @@
 from django .urls import path
-from . import views
+from .views import Dashboard, ExpenseDetail, ExpenseCreate, ExpenseUpdate, ExpenseDelete
+
 
 urlpatterns = [
-    path('', views.dashboard, name="dashboard")
+    # as_view is used to determine if the request is get or post
+    path('', Dashboard.as_view(), name="dashboard"),
+    path('expense/<int:pk>/', ExpenseDetail.as_view(), name='expense'),
+    path('expense-create/', ExpenseCreate.as_view(), name='expense-create'),
+    path('expense-update/<int:pk>/', ExpenseUpdate.as_view(), name='expense-update'),
+    path('expense-delete/<int:pk>/', ExpenseDelete.as_view(), name='expense-delete')
+
 ]
--- ExpenseTracker/base/views.py
+++ ExpenseTracker/base/views.py
@@ -1,7 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Expense
 
 # Tutorial Mode
 # Create your views here.
-def dashboard(request):
-    return HttpResponse('Hello World')
+
+class Dashboard(ListView):
+    model = Expense
+
+    # Overrides the default queryset name of 'object_list' into something that we choose.
+    # This affects that the html file looks for
+    context_object_name = 'expenses'
+
+    # Default is expense_list.html
+    template_name = 'base/dashboard.html'
+
+class ExpenseDetail(DetailView):
+    model = Expense
+    context_object_name = 'expense'
+
+    # Default is {expense}_detail.html
+    template_name = 'base/expense.html'
+
+# Default template is {expense}_form.html
+class ExpenseCreate(CreateView):
+    model = Expense
+
+    # Lists all the items in the field
+    # Can use this instead
+    #   fields = ['category', 'cost']
+    # Or alternatively, we can set our own ExpenseForm class
+    #   form_class = ExpenseForm
+    fields = '__all__'
+
+    # So if everything goes correctly, redirect user to the url named 'dashboard'
+    success_url = reverse_lazy('dashboard')
+
+# Default is {expense}_form.html
+class ExpenseUpdate(UpdateView):
+    model = Expense
+    fields = '__all__'
+    success_url = reverse_lazy('dashboard')
+
+# Default template is {expense}_confirm_delete.html
+class ExpenseDelete(DeleteView):
+    model = Expense
+    context_object_name = 'expense'
+    success_url = reverse_lazy('dashboard')
+
+
