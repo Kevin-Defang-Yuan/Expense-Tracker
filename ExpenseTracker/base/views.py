4a36002a2a294d7694a7a37451d3ddb8106a6d3a
Kevin Yuan <kyuan23@wooster.edu>
2023-01-06 23:00:20 -0800

    Initialize branch

--- ExpenseTracker/base/views.py
+++ ExpenseTracker/base/views.py
@@ -15,7 +15,7 @@ register = template.Library()
 
 
 # Branch
-# calendar
+# monthly
 LIM_NUM = 10
 MONTHS_NAME = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
