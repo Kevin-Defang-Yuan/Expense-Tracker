4f2b6f32ce7e7be7929b300b9a55949268cd88cb
Kevin Yuan <kyuan23@wooster.edu>
2023-02-09 19:28:34 -0500

    Added plus buttons to all lists

--- ExpenseTracker/base/filters.py
+++ ExpenseTracker/base/filters.py
@@ -1,22 +1,28 @@
 import django_filters
 from .models import Expense, Category
+from django.forms import TextInput
 
 # 
 def categories(request):
     return Category.objects.filter(user=request.user)
 
 class ExpenseFilter(django_filters.FilterSet):
-    description = django_filters.CharFilter(lookup_expr='icontains')
-    category = django_filters.ModelChoiceFilter(queryset=categories)
+    description = django_filters.CharFilter(lookup_expr='icontains', label="Description Contains:")
+    category = django_filters.ModelChoiceFilter(queryset=categories, label="Category:")
     o = django_filters.OrderingFilter(
         fields=(
             ('cost', 'cost'),
             ('date', 'date')
-        )
+        ),
+        label="Ordering"
     )
     class Meta:
         model = Expense
         fields = ['category']
+        widgets = {
+            'description': TextInput(attrs={'class': 'form-control'})
+        }
+        
         # fields = {
         #     'cost': ['lt', 'gt'],
         #     'date': ['exact', 'lt', 'gt'],
